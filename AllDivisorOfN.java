f7274e7a4f6eddb512af7184866851e8fdfd9539
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2021-03-02 01:18:39 +0530

    Create AllDivisorOfN.java

---
+++ AllDivisorOfN.java
@@ -0,0 +1,20 @@
+package Mathematics;
+
+public class AllDivisor {
+
+    static  void Divisor(int n){
+        int i;
+        for(i=1;i*i<=n;i++)
+        {
+            if(n%i==0)
+                System.out.print(i+" ");
+        }
+        
+        for(;i>=1;i--)
+            if(n%i==0)
+                System.out.print(n/i+" ");
+    }
+    public static void main(String[] args) {
+        Divisor(1000);
+    }
+}
